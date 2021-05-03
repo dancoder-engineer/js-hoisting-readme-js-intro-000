de7e52aa1fec66baba2a52301836c2d6f9daee88
DANIEL PAUL SEMINARA <danseminara@yahoo.com>
2021-05-03 21:47:24 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,10 +1,11 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
+  
 }
 
 
@@ -28,3 +29,4 @@ function sayMyName() {
     var name = "Kristin";
   }
 }
+console.log(callMe())
